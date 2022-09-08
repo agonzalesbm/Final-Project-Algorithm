ab28a64a30328d9546c8315bd5265e66d2c1fd37
andresgb <andres.gonzales.60093@usalesiana.edu.bo>
2022-09-08 02:37:29 -0400

    Added fixed RFile class

---
+++ lib/src/main/java/FinalProyect/RFile.java
@@ -0,0 +1,91 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+public class RFile {
+    DirectedGraph<User> directedGraph;
+    FileReader fileReader;
+    Scanner scanner;
+    public RFile() throws FileNotFoundException {
+        this.directedGraph = new DirectedGraph<>();
+        this.fileReader = new FileReader(Path.DATA.getGithub());
+        this.scanner = new Scanner(fileReader);
+    }
+    public void createUsers() { // n log n
+        User audie = new User(Integer.parseInt(scanner.nextLine()));
+        directedGraph.addNode(new Node<User>(audie));
+        int iterations = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < iterations; i++) { // n
+            String[] s = scanner.nextLine().split(" ");
+            int user1 = Integer.parseInt(s[0]);
+            int user2 = Integer.parseInt(s[1]);
+            directedGraph.addEdge((new User(user1)),(new User(user2)));
+        }
+        iterations = Integer.parseInt(scanner.nextLine());
+        topicList(iterations); // n log n
+        iterations = Integer.parseInt(scanner.nextLine());
+        repositoryList(iterations); //n log n
+        Node<User> user1 = directedGraph.getNode(new User(100));
+        System.out.println(user1.getValue().getTopicList());
+        System.out.println(user1.getValue().getRepositoriesFollowList());
+    }
+    public void topicList(int iterations) {
+        if (iterations == 0) {
+            return;
+        }
+        String nextLine = scanner.nextLine();
+        if (iterations > 0) {
+            String[] topics = nextLine.split(", ");
+            String[] getUser = topics[0].split(" ");
+            int userId = Integer.parseInt(getUser[0]);
+            Node<User> nodeUser = directedGraph.getNode(new User(userId));
+            String firstTopic = "";
+            if (getUser.length > 2) {
+                firstTopic = getUser[1] + " " + getUser[2];
+            } else {
+                firstTopic = getUser[1];
+            }
+            nodeUser.getValue().addTopic(firstTopic);
+            if (topics.length == 1) {
+                iterations--;
+                topicList(iterations);
+                return;
+            }
+            for (int i = 1; i < topics.length; i++) { // n
+                nodeUser.getValue().addTopic(topics[i]);
+            }
+            iterations--;
+            topicList(iterations); // log n
+            return;
+        }
+    }
+    public void repositoryList(int iterations) {
+        if (iterations == 0) {
+            return;
+        }
+        String nextLine = scanner.nextLine();
+        if (iterations > 0) {
+            String[] repositories = nextLine.split(", ");
+            String[] getUser = repositories[0].split(" ");
+            int userId = Integer.parseInt(getUser[0]);
+            Node<User> nodeUser = directedGraph.getNode(new User(userId));
+            String firstRepo = "";
+            if (getUser.length > 2) {
+                firstRepo = getUser[1] + " " + getUser[2];
+            } else {
+                firstRepo = getUser[1];
+            }
+            nodeUser.getValue().addRepositorie(firstRepo);
+            if (repositories.length == 1) {
+                iterations--;
+                repositoryList(iterations);
+                return;
+            }
+            for (int i = 1; i < repositories.length; i++) { // n
+                nodeUser.getValue().addRepositorie(repositories[i]);
+            }
+            iterations--;
+            repositoryList(iterations); // log n
+            return;
+        }
+    }
+}
