40123aff6402062544ffabacbc07f320eac30751
Esther Vasquez Villegas <esther.vasquez.60107@usalesiana.edu.bo>
2022-09-08 02:27:59 -0400

    All directed graph added

--- lib/src/main/java/FinalProyect/Node.java
+++ lib/src/main/java/FinalProyect/Node.java
@@ -1,91 +1,29 @@
 package FinalProyect;
 
-import java.util.LinkedList;
-import java.util.List;
 public class Node<T> {
-    private T value;
-    private List<Edge<T>> edgeList;
-    private boolean visited;
+    private final T value;
 
     public Node(T value) {
         this.value = value;
-        edgeList = new LinkedList<>();
-        visited = false;
     }
 
-    public boolean addEdge(Edge<T> newEdge) {
-        evaluateEdge(newEdge);
-        if (!edgeList.contains(newEdge)) {
-            edgeList.add(newEdge);
-            return true;
-        }
-        return false;
+    @Override
+    public boolean equals(Object o){
+        if (!(o instanceof Node)) return false;
+        return ((Node<?>) o).value.equals(this.value) || o == this;
     }
 
-    public boolean isConnectedWith(Node<T> node) {
-        for (Edge<T> edge : edgeList) {
-            if (edge.getDestination().equals(node)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public Edge<T> getEdgeWithConnectionTo(Node<T> node) {
-        if (isConnectedWith(node)) {
-            for (Edge<T> edge : edgeList) {
-                if (edge.getDestination().equals(node)) {
-                    return edge;
-                }
-            }
-        }
-        return null;
-    }
-
-    public List<Edge<T>> getEdgeList() {
-        return this.edgeList;
-    }
-
-    public boolean removeEdge(Edge<T> edg) {
-        if (edgeList.contains(edg)) {
-            edgeList.remove(edg);
-            return true;
-        }
-        return false;
-    }
-
-    public T getValue() {
-        return this.value;
-    }
-
-    public void visit() {
-        this.visited = true;
-    }
-
-    public boolean isVisited() {
-        return visited;
-    }
-
-    public void unVisitNode() {
-        this.visited = false;
-    }
-
-    public void evaluateEdge(Edge<T> edg) {
-        if (edg == null) {
-            throw new NullPointerException("Not valid null value");
-        }
-    }
-
-    public void evaluateGivenNode(Node<T> node) {
-        if (node == null) {
-            throw new NullPointerException("Not valid null value");
-        }
+    @Override
+    public int hashCode(){
+        return this.value.hashCode();
     }
 
     @Override
     public String toString() {
-        return "[value=" + value + "]";
+        return " " + this.value;
     }
 
-    
+    public T getValue() {
+        return value;
+    }
 }
