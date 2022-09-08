c84ba625c0e9324d001c916153e55f3ab7744edf
andresgb <andres.gonzales.60093@usalesiana.edu.bo>
2022-09-08 02:28:09 -0400

    Added RFile, User class to merge

--- lib/src/main/java/FinalProyect/RFile.java
+++ lib/src/main/java/FinalProyect/RFile.java
@@ -3,40 +3,34 @@ package FinalProyect;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
-
 public class RFile {
-    UndirectedGraph<User> undirectedGraph;
+    DirectedGraph<User> directedGraph;
     FileReader fileReader;
     Scanner scanner;
-
     public RFile() throws FileNotFoundException {
-        this.undirectedGraph = new UndirectedGraph<>();
+        this.directedGraph = new DirectedGraph<>();
         this.fileReader = new FileReader(Path.DATA.getGithub());
         this.scanner = new Scanner(fileReader);
     }
-
-    public void createUsers() throws FileNotFoundException { // n log n        
+    public void createUsers() { // n log n
         User audie = new User(Integer.parseInt(scanner.nextLine()));
-        undirectedGraph.addNode(new Node<User>(audie));
-
+        directedGraph.addNode(new Node<User>(audie));
         int iterations = Integer.parseInt(scanner.nextLine());
         for (int i = 0; i < iterations; i++) { // n
             String[] s = scanner.nextLine().split(" ");
             int user1 = Integer.parseInt(s[0]);
             int user2 = Integer.parseInt(s[1]);
-            undirectedGraph.addEdge(new Node<User>(new User(user1)), new Node<User>(new User(user2)));
+            directedGraph.addEdge((new User(user1)),(new User(user2)));
         }
-
         iterations = Integer.parseInt(scanner.nextLine());
         topicList(iterations); // n log n
         iterations = Integer.parseInt(scanner.nextLine());
-        repositorieList(iterations); //n log n
-        Node<User> user1 = undirectedGraph.getNode(new User(2002));
+        repositoryList(iterations); //n log n
+        Node<User> user1 = directedGraph.getNode(new User(100));
         System.out.println(user1.getValue().getTopicList());
         System.out.println(user1.getValue().getRepositoriesFollowList());
-    }    
-
-    public void topicList(int iterations) {        
+    }
+    public void topicList(int iterations) {
         if (iterations == 0) {
             return;
         }
@@ -45,7 +39,7 @@ public class RFile {
             String[] topics = nextLine.split(", ");
             String[] getUser = topics[0].split(" ");
             int userId = Integer.parseInt(getUser[0]);
-            Node<User> nodeUser = undirectedGraph.getNode(new User(userId));
+            Node<User> nodeUser = directedGraph.getNode(new User(userId));
             String firstTopic = "";
             if (getUser.length > 2) {
                 firstTopic = getUser[1] + " " + getUser[2];
@@ -53,7 +47,7 @@ public class RFile {
                 firstTopic = getUser[1];
             }
             nodeUser.getValue().addTopic(firstTopic);
-            if (topics.length == 1) {                
+            if (topics.length == 1) {
                 iterations--;
                 topicList(iterations);
                 return;
@@ -61,15 +55,12 @@ public class RFile {
             for (int i = 1; i < topics.length; i++) { // n
                 nodeUser.getValue().addTopic(topics[i]);
             }
-
-            iterations--;            
+            iterations--;
             topicList(iterations); // log n
             return;
         }
-        
     }
-
-    public void repositorieList(int iterations) {        
+    public void repositoryList(int iterations) {
         if (iterations == 0) {
             return;
         }
@@ -78,7 +69,7 @@ public class RFile {
             String[] repositories = nextLine.split(", ");
             String[] getUser = repositories[0].split(" ");
             int userId = Integer.parseInt(getUser[0]);
-            Node<User> nodeUser = undirectedGraph.getNode(new User(userId));
+            Node<User> nodeUser = directedGraph.getNode(new User(userId));
             String firstRepo = "";
             if (getUser.length > 2) {
                 firstRepo = getUser[1] + " " + getUser[2];
@@ -86,36 +77,17 @@ public class RFile {
                 firstRepo = getUser[1];
             }
             nodeUser.getValue().addRepositorie(firstRepo);
-            if (repositories.length == 1) {                
+            if (repositories.length == 1) {
                 iterations--;
-                repositorieList(iterations);
+                repositoryList(iterations);
                 return;
             }
             for (int i = 1; i < repositories.length; i++) { // n
                 nodeUser.getValue().addRepositorie(repositories[i]);
             }
-
-            iterations--;            
-            repositorieList(iterations); // log n
+            iterations--;
+            repositoryList(iterations); // log n
             return;
         }
-        
     }
-
-    public static void main(String[] args) throws FileNotFoundException {
-        RFile rFile = new RFile();
-        rFile.createUsers();
-        
-        // String string = "100 Java";
-        // String[] s = string.split(", ");
-        // String[] s = string.split(", ");
-        // System.out.println(Arrays.toString(s));
-
-    }
-
-    // public void followersAudieFollows(int N) {
-    //     for (Map.Entry<Node<User>, List<Edge<User>>> entry : undirectedGraph.getAdjList().entrySet()) {
-    //         System.out.println(entry.getValue());
-    //     }
-    // }
 }
