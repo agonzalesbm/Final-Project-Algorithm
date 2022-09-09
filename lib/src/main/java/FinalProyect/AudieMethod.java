54a0f96842cbb14c44e53a19d23421930540ac47
Esther Vasquez Villegas <esther.vasquez.60107@usalesiana.edu.bo>
2022-09-08 23:12:56 -0400

    Method 3 added

--- lib/src/main/java/FinalProyect/AudieMethod.java
+++ lib/src/main/java/FinalProyect/AudieMethod.java
@@ -1,8 +1,11 @@
 package FinalProyect;
 
 import java.io.FileNotFoundException;
+import java.util.*;
 
-public class AudieMethod {
+public class AudieMethod<T> {
+
+
     public void method1(){
     }
 
@@ -15,11 +18,41 @@ public class AudieMethod {
         System.out.println(rFile.getDirectedGraph());
     }
 
-    public void method3(){
+    public List<User> method3( User user1, int limit,DirectedGraph<User>directedGraph) {
+        Node<User> user = directedGraph.getNode(user1);
+        List<User> listUserFollowing = listFollowing(user, directedGraph);
+        List<User>listFinal = new LinkedList<>();
+        managUserList(limit-1, 0, user.getValue().getUserFollowList(), listFinal);
+        listFinal.removeAll(listUserFollowing);
+        listFinal.remove(user.getValue());
+        System.out.println(listFinal);
+        return listFinal;
+    }
+    private void managUserList(int limit, int cont, List<User>users, List<User>listFinal) {
+        if (limit == cont) {
+            listFinal.addAll(users);
+            return;
+        }
+        for (User user : users) {
+            managUserList(limit, cont + 1, user.getUserFollowList(),listFinal);
+        }
+    }
+    private List<User> listFollowing(Node<User> user,DirectedGraph<User>directedGraph) {
+        List<Edge<User>> edges = directedGraph.getAdjList().get(user);
+        List<User>listFollowing = new LinkedList<>();
+        for (Edge<User> edge : edges) {
+            listFollowing.add(edge.getDestination().getValue());
+        }
+        return listFollowing;
     }
 
     public static void main(String[] args) throws FileNotFoundException {
+        RFile rFile = new RFile();
+        rFile.createUsers();
         AudieMethod audieMethod = new AudieMethod();
-        audieMethod.method2(3);
+        User user1 = new User(100);
+        DirectedGraph<User> followers = rFile.getDirectedGraph();
+        audieMethod.method3(user1,2,followers);
+
     }
 }
